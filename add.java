a04e8ce24e1e3599834d66ca2e5a96e14c6ed874
ajayust <ajayv.ust@gmail.com>
2022-03-14 19:13:14 +0530

    version of file in new branch

--- add.java
+++ add.java
@@ -6,5 +6,23 @@ public class AddTwoNumbers {
       sum = num1 + num2;
 
       System.out.println("Sum of these numbers: "+sum);
-   }
+      System.out.println("demo project assignment for github");
+
+      Scanner sc=new Scanner(System.in);
+
+      System.out.print("Enter the first number: ");
+      int num1=sc.nextInt();
+      System.out.print("Enter the second number: ");
+      int num2=sc.nextInt();
+      sc.close();
+      sumNum(num1,num2);
+
+
+     }
+
+     public static void sumNum(int num_A,int num_B){
+        int sum=0;
+        sum=num_A+num_B;
+        System.out.println("Sum of two numbers  "+sum);
+        
 }
